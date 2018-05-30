5be236ee0ebe010724dc322ce481c64ee5ac602d
tongxingyi <tongxingyi>
2018-05-30 10:20:16 +0800

    提交项目

---
+++ src/com/oracle/assistutils/MD5.java
@@ -0,0 +1,42 @@
+package com.oracle.assistutils;
+
+
+import java.security.MessageDigest;
+/** 
+ * 说明：MD5处理
+ * 创建人：FH 
+ * 修改时间�?2014�?9�?20�?
+ * @version
+ */
+public class MD5 {
+
+	public static String md5(String str) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(str.getBytes());
+			byte b[] = md.digest();
+
+			int i;
+
+			StringBuffer buf = new StringBuffer("");
+			for (int offset = 0; offset < b.length; offset++) {
+				i = b[offset];
+				if (i < 0)
+					i += 256;
+				if (i < 16)
+					buf.append("0");
+				buf.append(Integer.toHexString(i));
+			}
+			str = buf.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		return str;
+	}
+	public static void main(String[] args) {
+		System.out.println(md5("31119@qq.com"+"123456"));
+		System.out.println(md5("mj1"));
+		System.out.println(md5("1"));
+	}
+}
---
+++ src/com/oracle/utils/GenerateUrlUtil.java
@@ -0,0 +1,30 @@
+package com.oracle.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.oracle.assistutils.MD5;
+
+import net.sf.json.JSONObject;
+
+public class GenerateUrlUtil {
+
+	private static final String IP = "http://117.36.53.108:8191/";
+
+	private static String path = "JYAppServerNew/apps/queryxajj/queryVehicleOrDlicence";
+
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("JYBH", "610643");
+		map.put("CXLB", "zhcx");
+		map.put("CXTJ", "6042");
+		JSONObject jsonStr = JSONObject.fromObject(map);
+		String token = MD5.md5(jsonStr.toString());
+		StringBuilder sb = new StringBuilder(IP);
+		sb.append(path);
+		sb.append("?jsonStr=" + jsonStr + "&token=" + token);
+		System.out.println(sb.toString());
+
+	}
+
+}
